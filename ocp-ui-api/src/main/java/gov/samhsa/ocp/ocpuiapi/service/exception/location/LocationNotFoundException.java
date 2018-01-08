717234d2e677ca948709a58cf2f9f3d1ef6343ab
Shruti Rao <shruti.rao@feisystems.com>
2018-01-08 15:20:55 -0500

    Add Exception class
    #17170:As a user, I can see a list of locations in the OCP, so that I know all of the locations in the system.

---
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/exception/location/LocationNotFoundException.java
@@ -0,0 +1,14 @@
+package gov.samhsa.ocp.ocpuiapi.service.exception.location;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class LocationNotFoundException extends RuntimeException {
+    public LocationNotFoundException() {
+        super();
+    }
+    public LocationNotFoundException(String message) {
+        super(message);
+    }
+}
