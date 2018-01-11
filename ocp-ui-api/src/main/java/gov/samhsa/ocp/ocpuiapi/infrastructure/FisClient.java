4de3fad8cf5cfc5416179eb331cdc3f41f196b1e
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-01-11 17:17:00 -0500

    Define methods to get practitioners from ocp-fis
    17311 - Create BFF for practitioners

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/infrastructure/FisClient.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/infrastructure/FisClient.java
@@ -1,6 +1,8 @@
 package gov.samhsa.ocp.ocpuiapi.infrastructure;
 
 import gov.samhsa.ocp.ocpuiapi.service.dto.LocationDto;
+import gov.samhsa.ocp.ocpuiapi.service.dto.PractitionerDto;
+import gov.samhsa.ocp.ocpuiapi.web.PractitionerController;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @FeignClient(name ="ocp-fis", url = "${ribbon.listOfServers}")
 public interface FisClient {
@@ -28,4 +31,16 @@ public interface FisClient {
 
     @RequestMapping(value = "/locations/{locationId}/childLocation", method = RequestMethod.GET)
     LocationDto getChildLocation(@PathVariable("locationId") String locationId);
+
+    @RequestMapping(value = "/practitioners", method = RequestMethod.GET)
+    List<PractitionerDto> getAllPractitioners(@RequestParam(value = "showInactive", required = false) boolean showInactive,
+                                              @RequestParam(value = "page", required = false) Integer page,
+                                              @RequestParam(value = "size", required = false) Integer size);
+
+    @RequestMapping(value = "/practitioners/search", method = RequestMethod.GET)
+    List<PractitionerDto> searchPractitioners(@RequestParam (value = "searchType", required = false) PractitionerController.SearchType searchType,
+                                              @RequestParam (value = "searchValue", required = false) String searchValue,
+                                              @RequestParam (value = "showInactive", required = false) boolean showInactive,
+                                              @RequestParam (value = "page", required = false) Integer page,
+                                              @RequestParam (value = "size", required = false) Integer size);
 }
