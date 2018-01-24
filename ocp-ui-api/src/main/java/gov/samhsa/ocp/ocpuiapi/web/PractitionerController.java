8acb9f63934f5d7ba23854ab937feaf62e8137c3
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-01-24 15:15:01 -0500

    Add getPractitioner and updatePractitioner in PractitionerController.
    #17174-As a user, I can enroll a new practitioner, so that practitioners can exist in the OCP.

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/web/PractitionerController.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/web/PractitionerController.java
@@ -10,7 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -90,6 +92,27 @@ public class PractitionerController {
             fisClient.createPractitioner(practitionerDto);
         } catch (FeignException fe) {
             ExceptionUtil.handleFeignExceptionRelatedToSearch(fe, " that the practitioner was not created", ResourceType.PRACTITIONER.name());
+        }
+    }
+
+    @PutMapping("/practitioners/{practitionerId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updatePractitioner(@PathVariable String practitionerId, @Valid @RequestBody PractitionerDto practitionerDto){
+        try{
+            fisClient.updatePractitioner(practitionerId,practitionerDto);
+
+        }catch (FeignException fe){
+            ExceptionUtil.handleFeignExceptionRelatedToResourceUpdate(fe, " that the practitioner was not updated", ResourceType.PRACTITIONER.name());
+        }
+    }
+
+    @GetMapping("/practitioners/{practitionerId}")
+    public PractitionerDto getPractitioner(@PathVariable String practitionerId){
+        try{
+            return fisClient.getPractitioner(practitionerId);
+        }catch(FeignException fe){
+            ExceptionUtil.handleFeignExceptionRelatedToSearch(fe,"NO practitioner was not found",ResourceType.PRACTITIONER.name());
+            return null;
         }
     }
 
