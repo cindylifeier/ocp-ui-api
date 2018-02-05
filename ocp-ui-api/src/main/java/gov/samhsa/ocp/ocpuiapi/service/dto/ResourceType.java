a102a67784fcdd7f4cea919602cb290ec205fb93
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-02-05 11:10:58 -0500

    Add a constant for CareTeam.
    17182 - As a user, I can create a care team for the patient with practitioner

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/dto/ResourceType.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/dto/ResourceType.java
@@ -1,5 +1,5 @@
 package gov.samhsa.ocp.ocpuiapi.service.dto;
 
 public enum ResourceType {
-    PATIENT,ORGANIZATION,PRACTITIONER,LOCATION;
+    PATIENT,ORGANIZATION,PRACTITIONER,LOCATION,CARE_TEAM;
 }
