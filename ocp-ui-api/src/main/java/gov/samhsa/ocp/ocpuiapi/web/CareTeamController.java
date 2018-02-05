2b1a1f81d40a0a6a5492ab539e698937b9e47ed9
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-02-05 13:52:45 -0500

    Add search care team on care team controller
    #17183-As a user, I can see a list of care teams for the patient, so I can know all of care teams the patient has.

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/web/CareTeamController.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/web/CareTeamController.java
@@ -3,23 +3,28 @@ package gov.samhsa.ocp.ocpuiapi.web;
 import feign.FeignException;
 import gov.samhsa.ocp.ocpuiapi.infrastructure.FisClient;
 import gov.samhsa.ocp.ocpuiapi.service.dto.CareTeamDto;
+import gov.samhsa.ocp.ocpuiapi.service.dto.PageDto;
 import gov.samhsa.ocp.ocpuiapi.service.dto.ResourceType;
 import gov.samhsa.ocp.ocpuiapi.util.ExceptionUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("ocp-fis/careteams")
+@RequestMapping("ocp-fis/careTeams")
 @Slf4j
 public class CareTeamController {
 
@@ -49,4 +54,23 @@ public class CareTeamController {
             ExceptionUtil.handleFeignExceptionRelatedToResourceUpdate(fe,"Care Team could not be updated in FHIR server", ResourceType.CARE_TEAM.name());
         }
     }
+
+    @GetMapping("/search")
+    public PageDto<CareTeamDto> searchCareTeams(@RequestParam(value="statusList",required = false) List<String> statusList,
+                                                @RequestParam(value="searchType",required = false) String searchType,
+                                                @RequestParam(value="searchValue",required = false) String searchValue,
+                                                @RequestParam(value="pageNumber",required = false) Integer pageNumber,
+                                                @RequestParam(value="pageSize",required = false) Integer pageSize){
+        log.info("Searching Care Teams from FHIR server");
+        try{
+            PageDto<CareTeamDto> careTeams=fisClient.searchCareTeams(statusList,searchType,searchValue,pageNumber,pageSize);
+            log.info("Got Response from FHIR server for Care Team Search");
+            return careTeams;
+        }catch(FeignException fe){
+            ExceptionUtil.handleFeignExceptionRelatedToSearch(fe,"No care Teams were found in configured FHIR server for the given searchType and searchValue", ResourceType.CARE_TEAM.name());
+            return null;
+        }
+
+    }
+
 }
