cf20d9172be0ae6f95c26ee2be3c3bb220d4c6b5
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-02-14 16:20:07 -0500

    Add required fiels in the DTO.
    17550 - As a user, I would like to add related person to patient

---
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/dto/RelatedPersonDto.java
@@ -0,0 +1,67 @@
+package gov.samhsa.ocp.ocpuiapi.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RelatedPersonDto {
+
+    private String relatedPersonId;
+
+    //identifier
+    private String identifierType;
+
+    private String identifierValue;
+
+    //active
+    private boolean active;
+
+    //patient
+    private String patient;
+
+    //relationship
+    private String relationshipCode;
+
+    private String relationshipValue;
+
+    //name
+    private String firstName;
+
+    private String lastName;
+
+    //telecom
+    private String telecomCode;
+
+    private String telecomValue;
+
+    //gender
+    private String genderCode;
+
+    private String genderValue;
+
+    //birthDate
+    private String birthDate;
+
+    //address
+    private String address1;
+
+    private String address2;
+
+    private String city;
+
+    private String state;
+
+    private String zip;
+
+    private String country;
+
+    //period
+    private String startDate;
+
+    private String endDate;
+}
