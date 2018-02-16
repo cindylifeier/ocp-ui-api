3cc08eca0e95141980c9f25aed7c4468713ca66a
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-02-16 17:15:09 -0500

    Create activity definition controller
    #17899:As a user, I can create activity definition at organization level

---
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/web/ActivityDefinitionController.java
@@ -0,0 +1,44 @@
+package gov.samhsa.ocp.ocpuiapi.web;
+
+import feign.FeignException;
+import gov.samhsa.ocp.ocpuiapi.infrastructure.FisClient;
+import gov.samhsa.ocp.ocpuiapi.service.dto.ActivityDefinitionDto;
+import gov.samhsa.ocp.ocpuiapi.service.dto.ResourceType;
+import gov.samhsa.ocp.ocpuiapi.util.ExceptionUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("ocp-fis")
+@Slf4j
+public class ActivityDefinitionController {
+    @Autowired
+    private FisClient fisClient;
+
+    @PostMapping("/organization/{organizationId}/activity-definitions")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createLocation(@PathVariable String organizationId,
+                               @Valid @RequestBody ActivityDefinitionDto activityDefinitionDto) {
+        log.info("About to create a location");
+        try {
+            fisClient.createActivityDefinition(organizationId, activityDefinitionDto);
+            log.info("Successfully created a location");
+        }
+        catch (FeignException fe) {
+            ExceptionUtil.handleFeignExceptionRelatedToResourceCreate(fe, " that the location was not created", ResourceType.LOCATION.name());
+        }
+    }
+
+
+
+}
