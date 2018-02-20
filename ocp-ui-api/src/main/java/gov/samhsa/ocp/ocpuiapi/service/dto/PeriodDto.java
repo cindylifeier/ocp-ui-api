0770dea86c859f4f120c8707caeac4c9f72350ef
mingfan <ming.fan@feisystems.com>
2018-02-20 09:56:49 -0500

    Change the field type of PeriodDto
    #17899: As a user, I can create activity definition at organization level

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/dto/PeriodDto.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/dto/PeriodDto.java
@@ -1,17 +1,20 @@
 package gov.samhsa.ocp.ocpuiapi.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class PeriodDto {
-    private LocalDate start;
-    private LocalDate end;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/YYYY")
+    private String start;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/YYYY")
+    private String end;
 }
