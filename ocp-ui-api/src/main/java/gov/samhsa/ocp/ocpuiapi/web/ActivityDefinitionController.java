ad18b226036596e16934b494cfd035196ea0469d
mingfan <ming.fan@feisystems.com>
2018-02-20 09:56:09 -0500

    Implement BFF to list activity definitions at organization level
    #17899: As a user, I can create activity definition at organization level

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/web/ActivityDefinitionController.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/web/ActivityDefinitionController.java
@@ -3,6 +3,7 @@ package gov.samhsa.ocp.ocpuiapi.web;
 import feign.FeignException;
 import gov.samhsa.ocp.ocpuiapi.infrastructure.FisClient;
 import gov.samhsa.ocp.ocpuiapi.service.dto.ActivityDefinitionDto;
+import gov.samhsa.ocp.ocpuiapi.service.dto.PageDto;
 import gov.samhsa.ocp.ocpuiapi.service.dto.ResourceType;
 import gov.samhsa.ocp.ocpuiapi.util.ExceptionUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("ocp-fis")
@@ -24,6 +27,25 @@ import javax.validation.Valid;
 public class ActivityDefinitionController {
     @Autowired
     private FisClient fisClient;
+
+    @GetMapping("/organizations/{organizationId}/activity-definitions")
+    public PageDto<ActivityDefinitionDto> getAllActivityDefinitionsByOrganization(@PathVariable String organizationId,
+                                                                                @RequestParam(value = "searchKey", required = false) String searchKey,
+                                                                                @RequestParam(value = "searchValue", required = false) String searchValue,
+                                                                                @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+                                                                                @RequestParam(value = "pageSize", required = false) Integer pageSize) {
+        log.info("Fetching activity definitions from FHIR Server for the given OrganizationId: " + organizationId);
+        try {
+            PageDto<ActivityDefinitionDto> fisClientResponse = fisClient.getAllActivityDefinitionsByOrganization(organizationId, searchKey, searchValue, pageNumber, pageSize);
+            log.info("Got response from FHIR Server...");
+            return fisClientResponse;
+        }
+        catch (FeignException fe) {
+            ExceptionUtil.handleFeignExceptionRelatedToSearch(fe, "no activity definitions were found in the configured FHIR server for the given OrganizationId", ResourceType.ORGANIZATION.name());
+            return null;
+        }
+    }
+
 
     @PostMapping("/organization/{organizationId}/activity-definitions")
     @ResponseStatus(HttpStatus.CREATED)
