8976575cef969ed7e6ec558c8c83b800f31d8185
mingfan <ming.fan@feisystems.com>
2018-03-05 16:50:02 -0500

    Implement to list Communication in OCP-UI-API
    #18345: As a user I should be able to create and edit communications with patient so I can check my notes later - Functionality Implementation

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/web/CommunicationController.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/web/CommunicationController.java
@@ -8,15 +8,18 @@ import gov.samhsa.ocp.ocpuiapi.util.ExceptionUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("ocp-fis")
@@ -24,6 +27,24 @@ import javax.validation.Valid;
 public class CommunicationController {
     @Autowired
     FisClient fisClient;
+
+    @GetMapping("/communications/search")
+    public Object getCommunications(@RequestParam(value = "statusList", required = false) List<String> statusList,
+                                  @RequestParam(value = "searchKey", required = false) String searchKey,
+                                  @RequestParam(value = "searchValue", required = false) String searchValue,
+                                  @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+                                  @RequestParam(value = "pageSize", required = false) Integer pageSize) {
+        log.info("Searching Communications from FHIR server");
+        try {
+            Object communication = fisClient.getCommunications(statusList, searchKey, searchValue, pageNumber, pageSize);
+            log.info("Got Response from FHIR server for Communications Search");
+            return communication;
+        } catch (FeignException fe) {
+            ExceptionUtil.handleFeignExceptionRelatedToSearch(fe, "No Communications were found in configured FHIR server for the given searchKey and searchValue");
+            return null;
+        }
+    }
+
 
     @PostMapping("/communications")
     @ResponseStatus(HttpStatus.CREATED)
