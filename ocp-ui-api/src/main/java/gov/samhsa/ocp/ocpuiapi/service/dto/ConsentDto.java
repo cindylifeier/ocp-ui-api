fcf4c6f4cb42c00ed36dbb282e9fde1f76b7c04a
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-04-02 13:07:03 -0400

    Convert localdatetime to localdate
    #18779:As a patient I should be able to create a general designation consent so I can let my Care team members have access to my health record

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/dto/ConsentDto.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/dto/ConsentDto.java
@@ -1,17 +1,20 @@
 package gov.samhsa.ocp.ocpuiapi.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ConsentDto {
 
     private String logicalId;
@@ -20,7 +23,8 @@ public class ConsentDto {
 
     private PeriodDto period;
 
-    private LocalDateTime dateTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM/dd/yyyy")
+    private LocalDate dateTime;
 
     private ValueSetDto status;
 
