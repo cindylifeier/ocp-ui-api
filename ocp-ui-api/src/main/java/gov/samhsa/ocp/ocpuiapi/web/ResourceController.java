ac54b796967549bc2f916ffdaffde26ed79067a1
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-04-23 14:14:34 -0400

    Add ocp-fis in controller

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/web/ResourceController.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/web/ResourceController.java
@@ -5,9 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("ocp-fis")
 @Slf4j
 public class ResourceController {
     @Autowired
