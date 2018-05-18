44f229a674bc410da24b836d967afe0e73ca6da7
Shruti Rao <shruti.rao@feisystems.com>
2018-05-18 15:28:52 -0400

    Add Calendar DTOs
    #18450:[Spike] Research API integration of Outlook

---
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/dto/EwsCalendarDto.java
@@ -0,0 +1,35 @@
+package gov.samhsa.ocp.ocpuiapi.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EwsCalendarDto {
+    private String subject;
+    private Date start;
+    private Date end;
+    private String location;
+    private String organizerEmail;
+    private String organizerName;
+    private List<NameAndEmailAddressDto> requiredAttendees;
+    private List<NameAndEmailAddressDto> optionalAttendees;
+    private double durationInMinutes;
+    private String timeZone;
+
+    private String myResponse;
+    private String calUid;
+
+    private boolean allDayEvent;
+    private boolean cancelled;
+    private boolean meeting;
+    private boolean recurring;
+    private boolean responseRequested;
+}
