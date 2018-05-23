1a5961c459e0307386b86091b8e88684e629b127
burcakulug <burcak.ulug@feisystems.com>
2018-05-22 21:44:15 -0400

    Refactor oauth2 configuration and utilize one property object
    #19878:As a Developer, Implement launch context handling in Authorization Server for standalone SMART on FHIR launch - Continued

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/config/ConfigProperties.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/config/ConfigProperties.java
@@ -1,5 +1,9 @@
 package gov.samhsa.ocp.ocpuiapi.config;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -7,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.io.IOException;
 
 @Component
 @ConfigurationProperties(prefix = "ocp.ocp-ui-api")
@@ -19,7 +24,28 @@ public class ConfigProperties {
     @Data
     public static class OAuth2 {
         @Valid
+        @NotNull
+        public OAuth2Client oauth2Client;
+        @Valid
         @NotBlank
         private String authorizationServerEndpoint;
     }
+
+    @Data
+    public static class OAuth2Client {
+        @NotBlank
+        private String clientId;
+
+        @NotBlank
+        @JsonSerialize(using = MaskedSerializer.class)
+        private String clientSecret;
+    }
+
+    public static class MaskedSerializer extends JsonSerializer<String> {
+        @Override
+        public void serialize(String s, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeString("********");
+        }
+    }
+
 }
