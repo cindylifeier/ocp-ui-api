4bb1ead38151cb7209e0a69fcc953d1ff5ec9852
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-06-11 15:44:40 -0400

    Create a Controller for coverage.
    #20218:As a User I will be able to capture Patient's coverage information - Wireframes and Functionality.

---
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/web/CoverageController.java
@@ -0,0 +1,38 @@
+package gov.samhsa.ocp.ocpuiapi.web;
+
+import feign.FeignException;
+import gov.samhsa.ocp.ocpuiapi.infrastructure.FisClient;
+import gov.samhsa.ocp.ocpuiapi.service.dto.CoverageDto;
+import gov.samhsa.ocp.ocpuiapi.service.dto.ReferenceDto;
+import gov.samhsa.ocp.ocpuiapi.util.ExceptionUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("ocp-fis")
+@Slf4j
+public class CoverageController {
+
+    @Autowired
+    private FisClient fisClient;
+
+    @PostMapping("/coverage")
+    public void createCoverage(@Valid @RequestBody CoverageDto coverageDto){
+        log.info("About to create a coverage");
+        try{
+            fisClient.createCoverage(coverageDto);
+            log.info("Successfully created a coverage");
+        }catch(FeignException fe){
+            ExceptionUtil.handleFeignException(fe,"that the coverage was not created");
+        }
+    }
+}
