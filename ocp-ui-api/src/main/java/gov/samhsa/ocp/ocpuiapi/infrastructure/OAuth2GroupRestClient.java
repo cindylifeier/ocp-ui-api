a85953886c79bf7436419334f20bd66dc449b07e
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-06-21 16:34:09 -0400

    Use Dto when calling the UAA groups endpoint
    20389 - Assign Permissions Group

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/infrastructure/OAuth2GroupRestClient.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/infrastructure/OAuth2GroupRestClient.java
@@ -1,19 +1,17 @@
 package gov.samhsa.ocp.ocpuiapi.infrastructure;
 
 import gov.samhsa.ocp.ocpuiapi.config.OAuth2FeignClientCredentialsConfig;
+import gov.samhsa.ocp.ocpuiapi.service.dto.uaa.group.GroupWrapperDto;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 @FeignClient(name = "oauth2GroupRestClient", url = "${ocp.ocp-ui-api.oauth2.authorization-server-endpoint}", configuration = OAuth2FeignClientCredentialsConfig.class)
 public interface OAuth2GroupRestClient {
 
-    @RequestMapping(value = "/Groups", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    Object getAllGroups();
+    @RequestMapping(value = "/Groups?count=1000", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    GroupWrapperDto getAllGroups();
 
     @RequestMapping(value = "/Users", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     Object getAllUsersByOrganization();
