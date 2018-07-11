eeba1960896ae401f2d5a5a2026a2348289fdc9d
burcakulug <burcak.ulug@feisystems.com>
2018-07-11 11:50:52 -0400

    Force use of client credentials when in oAuth2FeignRequestInterceptor
    #20389: As a Org Admin, I should be able to assign permission groups to all users of my organization

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/config/OAuth2FeignClientCredentialsConfig.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/config/OAuth2FeignClientCredentialsConfig.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.OAuth2ClientProperties;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 
 // DO NOT add @Configuration unless this needs to globally apply to all Feign Clients
 public class OAuth2FeignClientCredentialsConfig {
@@ -23,7 +28,22 @@ public class OAuth2FeignClientCredentialsConfig {
     }
 
     @Bean
-    public OAuth2FeignRequestInterceptor oAuth2FeignRequestInterceptor(OAuth2ClientContext oAuth2ClientContext, ClientCredentialsResourceDetails resource) {
-        return new OAuth2FeignRequestInterceptor(oAuth2ClientContext, resource);
+    @Scope(value = "request", proxyMode = ScopedProxyMode.INTERFACES)
+    public DefaultOAuth2ClientContext oauth2ClientCredentialsContext() {
+        final DefaultOAuth2ClientContext context = new DefaultOAuth2ClientContext(
+                new DefaultAccessTokenRequest());
+        return context;
+    }
+
+    /**
+     * Hard-wire {@link #oauth2ClientCredentialsContext} bean here to force the use of client credentials.
+     * Otherwise, if an {@link OAuth2Authentication} exists in the {@link SecurityContextHolder}, the access token
+     * from that will be used instead of client credentials.
+     * See the default configuration below as reference:
+     * {@linkplain org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2RestOperationsConfiguration.RequestScopedConfiguration#oauth2ClientContext}
+     */
+    @Bean
+    public OAuth2FeignRequestInterceptor oAuth2FeignRequestInterceptor(ClientCredentialsResourceDetails resource) {
+        return new OAuth2FeignRequestInterceptor(oauth2ClientCredentialsContext(), resource);
     }
 }
