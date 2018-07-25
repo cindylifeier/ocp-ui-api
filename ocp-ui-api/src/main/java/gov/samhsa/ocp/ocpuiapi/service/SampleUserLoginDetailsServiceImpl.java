e910e35b9e7d5467bafe90d6c68746dd025013e9
JiahaoLi <Jiahao.Li@feisystems.com>
2018-07-24 20:33:22 -0400

    Rename UserLoginDetails to SampleUserLoginDetails
    #20664: Update Demo environment with QA code by end of this sprint

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/SampleUserLoginDetailsServiceImpl.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/SampleUserLoginDetailsServiceImpl.java
@@ -1,7 +1,7 @@
 package gov.samhsa.ocp.ocpuiapi.service;
 
 import gov.samhsa.ocp.ocpuiapi.config.SampleFileProperties;
-import gov.samhsa.ocp.ocpuiapi.service.dto.UserLoginDetailsDto;
+import gov.samhsa.ocp.ocpuiapi.service.dto.SampleUserLoginDetailsDto;
 import gov.samhsa.ocp.ocpuiapi.service.exception.NoDocumentsFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
@@ -15,7 +15,7 @@ import java.io.InputStream;
 
 @Service
 @Slf4j
-public class UserLoginDetailsServiceImpl implements UserLoginDetailsService {
+public class SampleUserLoginDetailsServiceImpl implements SampleUserLoginDetailsService {
 
     private static final String SAMPLE_DOCUMENTS_FOLDER = "sampledocs/";
 
@@ -23,15 +23,15 @@ public class UserLoginDetailsServiceImpl implements UserLoginDetailsService {
     SampleFileProperties sampleFileProperties;
 
     @Override
-    public UserLoginDetailsDto getUserLoginDetails() {
-        UserLoginDetailsDto userLoginDetailsDto = new UserLoginDetailsDto();
+    public SampleUserLoginDetailsDto getSampleUserLoginDetails() {
+        SampleUserLoginDetailsDto sampleUserLoginDetailsDto = new SampleUserLoginDetailsDto();
         String sampleFilePath = SAMPLE_DOCUMENTS_FOLDER.concat(sampleFileProperties.getSampleFileForLoginDetails().getFileName());
         ClassPathResource classPathResource = new ClassPathResource(sampleFilePath);
         try (InputStream sampleFileInputStream = classPathResource.getInputStream()) {
             byte[] sampleFileBytes = IOUtils.toByteArray(sampleFileInputStream);
             String encodedBase64 = new String(Base64.encodeBase64(sampleFileBytes));
-            userLoginDetailsDto.setEncodedPdf(encodedBase64);
-            return userLoginDetailsDto;
+            sampleUserLoginDetailsDto.setEncodedPdf(encodedBase64);
+            return sampleUserLoginDetailsDto;
         } catch (IOException e) {
             log.error("Unable to get sample file: " + sampleFilePath);
             log.debug(e.getMessage(), e);
--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/dto/SampleUserLoginDetailsDto.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/dto/SampleUserLoginDetailsDto.java
@@ -3,6 +3,6 @@ package gov.samhsa.ocp.ocpuiapi.service.dto;
 import lombok.Data;
 
 @Data
-public class UserLoginDetailsDto {
+public class SampleUserLoginDetailsDto {
     String encodedPdf;
 }
