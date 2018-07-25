39e35c0e4c131bc4919deb973af389632034bfdd
JiahaoLi <Jiahao.Li@feisystems.com>
2018-07-24 20:21:48 -0400

    Implement to configure sample file for user login details
    #20664: Update Demo environment with QA code by end of this sprint

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/UserLoginDetailsServiceImpl.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/UserLoginDetailsServiceImpl.java
@@ -1,34 +1,41 @@
 package gov.samhsa.ocp.ocpuiapi.service;
 
+import gov.samhsa.ocp.ocpuiapi.config.SampleFileProperties;
 import gov.samhsa.ocp.ocpuiapi.service.dto.UserLoginDetailsDto;
+import gov.samhsa.ocp.ocpuiapi.service.exception.NoDocumentsFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 @Slf4j
 public class UserLoginDetailsServiceImpl implements UserLoginDetailsService {
 
-    private static final String SAMPLE_USER_LOGIN_DETAILS_PDF = "sample-user-login-details.pdf";
+    private static final String SAMPLE_DOCUMENTS_FOLDER = "sampledocs/";
+
+    @Autowired
+    SampleFileProperties sampleFileProperties;
 
     @Override
     public UserLoginDetailsDto getUserLoginDetails() {
         UserLoginDetailsDto userLoginDetailsDto = new UserLoginDetailsDto();
-        try {
-            File file = new ClassPathResource(SAMPLE_USER_LOGIN_DETAILS_PDF).getFile();
-            FileInputStream fileInputStreamReader = new FileInputStream(file);
-            byte[] bytes = new byte[(int) file.length()];
-            fileInputStreamReader.read(bytes);
-            String encodedBase64 = new String(Base64.encodeBase64(bytes));
+        String sampleFilePath = SAMPLE_DOCUMENTS_FOLDER.concat(sampleFileProperties.getSampleFileForLoginDetails().getFileName());
+        ClassPathResource classPathResource = new ClassPathResource(sampleFilePath);
+        try (InputStream sampleFileInputStream = classPathResource.getInputStream()) {
+            byte[] sampleFileBytes = IOUtils.toByteArray(sampleFileInputStream);
+            String encodedBase64 = new String(Base64.encodeBase64(sampleFileBytes));
             userLoginDetailsDto.setEncodedPdf(encodedBase64);
+            return userLoginDetailsDto;
         } catch (IOException e) {
-            log.info("No source pdf file found." + e);
+            log.error("Unable to get sample file: " + sampleFilePath);
+            log.debug(e.getMessage(), e);
+            throw new NoDocumentsFoundException("Unable to get sample file for login details");
         }
-        return userLoginDetailsDto;
     }
 }
