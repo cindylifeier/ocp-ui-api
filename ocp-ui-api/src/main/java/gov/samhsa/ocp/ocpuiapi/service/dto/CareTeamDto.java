3643f535cffab8fba8d6fe35f0899da4e90683f2
Utish Rajkarnikar <utish.rajkarnikar@feisystems.com>
2018-09-03 22:01:32 -0400

    Add two new fields in Coverage
    21311 - Bring UAA roles for practitioners

--- ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/dto/CareTeamDto.java
+++ ocp-ui-api/src/main/java/gov/samhsa/ocp/ocpuiapi/service/dto/CareTeamDto.java
@@ -40,4 +40,8 @@ public class CareTeamDto {
     private List<ParticipantDto> participants;
 
     private String managingOrganization;
+
+    private String episodeOfCareCode;
+
+    private String episodeOfCareType;
 }
